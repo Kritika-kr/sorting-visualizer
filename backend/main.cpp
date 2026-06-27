747c8a34ed92a9a01e0eee11f51b7d4099ec67a2
Kritika Kumari <kritika3073@gmail.com>
2026-06-27 14:16:48 +0530

    added files

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,9 +1,16 @@
+#include <cstdlib>
 #include <drogon/drogon.h>
 
-int main() {
-  drogon::app()
-      .addListener("0.0.0.0", 8080)
-      .setThreadNum(4)
-      .enableSession(false)
-      .run();
+int main()
+{
+    int port = 8080;
+
+    if(const char* env = std::getenv("PORT"))
+        port = std::stoi(env);
+
+    drogon::app()
+        .addListener("0.0.0.0", port)
+        .setThreadNum(4)
+        .enableSession(false)
+        .run();
 }
